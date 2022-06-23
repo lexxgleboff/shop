0fe73287532175bad2f7f73c646f84cf7c6a4117
lexxgleboff <64305994+lexxgleboff@users.noreply.github.com>
2022-06-23 12:02:50 +0300

    Вынес генерацию JWT токена в отдельную функцию

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,15 @@
 const ApiError = require('../error/ApiError')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const {User, Basket} = require('../models/models')
+const { User, Basket } = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+            { id, email, role },
+            process.env.SECRET_KEY,
+            {expiresIn: '12h'}
+        )
+}
 
 class UserController {
     async registration(req, res, next) {
@@ -16,11 +24,7 @@ class UserController {
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({ email, role, password: hashPassword })
         const basket = await Basket.create({ userId: user.id })
-        const token = jwt.sign(
-            { id: user.id, email, role },
-            process.env.SECRET_KEY,
-            {expiresIn: '12h'}
-        )
+        const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
 
