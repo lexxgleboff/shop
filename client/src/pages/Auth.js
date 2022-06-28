75ac769553cc615e85e22e043cd288a485f51aea
lexxgleboff <64305994+lexxgleboff@users.noreply.github.com>
2022-06-28 18:16:17 +0300

    Создал форму авторизации и регистрации

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import { Container, Form } from 'react-bootstrap';
+import { Button, Card, Container, Form } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
         <Container
             className='d-flex justify-content-center align-items-center'
             style={{height: window.innerHeight - 56}}
         >
-            <Form>
-                
-            </Form>
+            <Card style={{width: 500}} className='p-4'>
+                <h2 className='m-auto'>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                <Form className='d-flex flex-column'>
+                    <Form.Control
+                        className='mt-3'
+                        placeholder='Введите ваш email'
+                    />
+                    <Form.Control
+                        className='mt-3'
+                        placeholder='Введите ваш пароль'
+                    />
+                    <div className='d-flex justify-content-between align-items-center mt-3'>
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйстя!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        }
+                        <Button
+                            variant={'outline-success'}
+                        >
+                            {isLogin ? 'Войти' : 'Регистрация'}
+                        </Button> 
+                    </div>
+
+                </Form>
+            </Card>
         </Container>
     );
 };
