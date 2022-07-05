23b2f6c6df3299535921e3f83bec039586d9cf18
lexxgleboff <64305994+lexxgleboff@users.noreply.github.com>
2022-07-05 12:10:12 +0300

    Исправил форму добавления устройства в админке

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { Button, Form, FormControl } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Button, Dropdown, Form, FormControl } from 'react-bootstrap';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import { Context } from '../..';
 
 const CreateDevice = () => {
+    const { device } = useContext(Context)
+    // const [info, setInfo] = useState([])
     return (
         <Form>
+            <Dropdown className='mb-2'>
+                <DropdownToggle variant='outline-secondary'>Выберите тип</DropdownToggle>
+                <DropdownMenu>
+                    {device.types.map(type => 
+                        <DropdownItem key={type.id}>{type.name}</DropdownItem>
+                    )}
+                </DropdownMenu>
+            </Dropdown>
+            <Dropdown className='mb-2'>
+                <DropdownToggle variant='outline-secondary'>Выберите бренд</DropdownToggle>
+                <DropdownMenu>
+                    {device.brands.map(brand => 
+                        <DropdownItem key={brand.id}>{brand.name}</DropdownItem>
+                    )}
+                </DropdownMenu>
+            </Dropdown>
             <FormControl
-                placeholder={'Введите название девайса'}    
+                placeholder={'Введите название девайса'}
+                className='mb-2'
+            >         
+            </FormControl>    
+            <FormControl
+                placeholder={'Введите название девайса'}
+                type='number'
+                className='mb-2'
+            >         
+            </FormControl>    
+            <FormControl
+                placeholder={'Введите название девайса'}
+                type='file'
+                className='mb-2'
             >         
             </FormControl>    
             <div className='d-flex justify-content-end'>
